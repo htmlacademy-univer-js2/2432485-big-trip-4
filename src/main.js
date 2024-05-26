844704c84f6c69fda0b5fb0769c81aff37d59cc7
Ксения Смолякова <smalyokova.ksenya.05@mail.ru>
2024-05-26 23:32:19 +0500

    Дополнительная функциональность

--- src/main.js
+++ src/main.js
@@ -5,10 +5,10 @@ import PointsModel from './model/points-model.js';
 import FilterModel from './model/filter-model.js';
 import FilterPresenter from './presenter/filter-presenter.js';
 import NewPointButtonView from './view/new-point-button-view.js';
-import PointsApiService from './points-api-service.js';
+import PointsApiService from './points-api-service.js'; //
 import OffersModel from './model/offers-model.js';
 import DestinationsModel from './model/destinations-model.js';
-import { AUTHORIZATION, END_POINT } from './constants.js';
+import { AUTHORIZATION, END_POINT } from './mock/constants.js';
 
 const siteMainElement = document.querySelector('.page-main');
 const siteHeaderElement = document.querySelector('.trip-main');
@@ -24,7 +24,7 @@ const filterModel = new FilterModel();
 const filterPresenter = new FilterPresenter(siteHeaderElement.querySelector('.trip-controls__filters'), filterModel, pointsModel);
 filterPresenter.init();
 
-const tripPresenter = new TripPresenter(siteMainElement.querySelector('.trip-events'), pointsModel, filterModel, destinationsModel, offersModel);
+const tripPresenter = new TripPresenter(siteHeaderElement.querySelector('.trip-main__trip-info'), siteMainElement.querySelector('.trip-events'), pointsModel, filterModel, destinationsModel, offersModel);
 tripPresenter.init();
 
 const handleNewPointFormClose = () => {
